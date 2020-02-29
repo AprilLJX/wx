fe03e6042895eb9cbe91ca56ba7c087919a0e7ba
Jacyn <liujiaxin@pku.edu.cn>
2020-02-29 18:08:57 +0800

    第一个小程序

--- myMIniProgram1/myCode/pages/index/index.js
+++ myMIniProgram1/myCode/pages/index/index.js
@@ -1,5 +1,5 @@
 // pages/index/index.js
-Component({
+Page({
   /**
    * 组件的属性列表
    */
@@ -11,7 +11,16 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    name: "hello world",
+    img: "/images/pic1.jpeg"
+  },
+  getMyInfo:function(e){
+    console.log(e.detail.userInfo)
+    let info = e.detail.userInfo;
+    this.setData({
+      name: info.nickName,
+      img: info.avatarUrl
+    })
   },
 
   /**
